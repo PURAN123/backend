e01cca3fb13e5ae94ce56240978d1df3381e8337
puranchandra <puran@exlitem.com>
2025-08-17 23:29:21 +0530

    Update ALLOWED_HOSTS to allow all hosts in settings.py; refresh compiled Python files in __pycache__ directories.

--- backend/settings.py
+++ backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-4$tdkyr9kghjkk^8be6dnoz0@ui%u2-sqj)3o95r(cog6_hkjw
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
